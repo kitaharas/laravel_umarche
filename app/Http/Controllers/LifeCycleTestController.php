32552ece1929d7336f386bb8737779c5792208f2
kitaharas <mizupe1223@gmail.com>
2022-05-15 14:29:37 +0900

    サービスプロバイダのテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 class LifeCycleTestController extends Controller
 {
     //
+    public function showServiceProviderTest()
+    {
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('passwords');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
+    
     public function showServiceContainerTest()
     {
         app()->bind('lifeCycleTest',function(){
@@ -20,7 +30,7 @@ class LifeCycleTestController extends Controller
         // $sample = new Sample($message);
         // $sample->run();
 
-        // サービスコンテナありのパターン
+        // サービスコンテナapp()ありのパターン
         app()->bind('sample',Sample::class);
         $sample = app()->make('sample');
         $sample->run();
